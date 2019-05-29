297bf64d293da72f043637fa9cedf03e7830539e
eserdk <eserdk@gmail.com>
2019-05-29 22:21:25 +0300

    remove outdated coordinates

--- app/calculation.py
+++ app/calculation.py
@@ -1,3 +1,13 @@
+def remove_outdated_coordinates(coordinates):
+    i_oldest = 0
+    date_oldest = coordinates[0]['datetime']
+    for i in range(1, len(coordinates)):
+        if date_oldest < coordinates[i]['datetime']:
+            date_oldest = coordinates[i]['datetime']
+            i_oldest = i
+    del coordinates[i_oldest]
+
+
 def calculate_lives(shape):
     return 5
 
--- app/sock.py
+++ app/sock.py
@@ -1,8 +1,9 @@
+import datetime
 import json
 from flask import request
 from flask_socketio import emit
 
-from app.calculation import calculate_lives
+from app.calculation import calculate_lives, remove_outdated_coordinates
 
 from . import socketio
 from .memory import redis
@@ -13,6 +14,10 @@ def joined(data):
     user_id = request.sid
     data = json.loads(data)
     data['lives'] = calculate_lives(data['shape'])
+    data['coordinates'] = [{
+        'datetime': datetime.datetime.utcnow(),
+        'position': data['coordinates']
+    }, ]
     redis.set(user_id, json.dumps(data))
     response = {'user_id': user_id, 'data': data}
     emit('joined', response)
@@ -21,6 +26,15 @@ def joined(data):
 @socketio.on('move')
 def moved(data):
     user_id = request.sid
+    data = json.loads(data)
+    if len(data['coordinates']) == 3:
+        remove_outdated_coordinates(data['coordinates'])
+
+    data['coordinates'].append({
+        'datetime': datetime.datetime.utcnow(),
+        'position': data['coordinates']
+    })
     redis.set(user_id, data)
-    response = [{'user_id': k, 'data': json.loads(redis.get(k))} for k in redis.keys(f'^(?!{user_id})$')]
+    response = [{'user_id': k, 'data': json.loads(redis.get(k))} for k in
+                redis.keys(f'^(?!{user_id})$')]
     emit('moved', response)
